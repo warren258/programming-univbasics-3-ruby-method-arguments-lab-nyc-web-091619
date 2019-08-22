b1c0a6a99fae690321836dbaf6d6c95c652434c4
Warren McDonald <warrenjmac@mail.com>
2019-08-22 19:53:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,8 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
   end
   
-def   
+def introduction_with_language_optional(name, language = "Ruby")
+  puts 
 
 
 
